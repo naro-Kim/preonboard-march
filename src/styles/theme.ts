392496a6496adbaecd0dbdf979e056a7b3df4729
naro_Kim <gracias9022@gmail.com>
2024-02-29 01:06:12 +0900

    style: add css theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+  color: {
+    primary: '#2376FF',
+    warning: '#F2330F',
+    gray50: '#FEFEFE',
+    gray100: '#F7F7FC',
+    gray200: '#EFF0F6',
+    gray300: '#D9DBE9',
+    gray400: '#BEC1D5',
+    gray500: '#A0A3BD',
+    gray600: '#6E7191',
+    gray700: '#4E4B66',
+    gray800: '#2A2A44',
+    gray900: '#14142B',
+    white: '#FEFEFE',
+    black: '#222222',
+    body: '#202B3D',
+    caption: '#636C79',
+  },
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const ADD_BUTTON_TEXT = 'Add';
+export const DELETE_BUTTON_TEXT = 'Delete';
